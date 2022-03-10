5f0509bf0abb3faae98f3cd8b6741aea77ae16c3
Keks <keks@htmlacademy.ru>
2022-03-10 21:01:50 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/main.js
+++ 3/js/main.js
@@ -1,7 +1,6 @@
-/* eslint-disable no-console */
-const getRandomPositiveInteger = (a, b) => {
-  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+const getRandomPositiveInteger = ( numberA, numberB) => {
+  const lower = Math.ceil(Math.min(Math.abs(numberA), Math.abs(numberB)));
+  const upper = Math.floor(Math.max(Math.abs(numberA), Math.abs(numberB)));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
 };
