8b7462eb3f5cbe3b49f45d80eec76379bc05fd92
Keks <keks@htmlacademy.ru>
2022-03-10 21:43:40 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/main.js
@@ -0,0 +1,4 @@
+import {getTestPost} from './post.js';
+const getTestPosts =  Array.from({length: 25}, (v,i) => getTestPost(i+1));
+// eslint-disable-next-line no-console
+console.log(getTestPosts);
