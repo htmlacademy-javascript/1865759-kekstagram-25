6c818b3b02d9f5bb8e50b5e079dec9683dcd62f8
Елена <el_el_@mail.ru>
2022-03-10 21:17:04 +0300

    Больше деталей3

--- js/main.js
+++ js/main.js
@@ -48,4 +48,5 @@ const getTestPost = (value) => ({
 });
 
 const getTestPosts = Array.from({length: 25}, (v,i) => getTestPost(i+1));
+// eslint-disable-next-line no-console
 console.log(getTestPosts);
