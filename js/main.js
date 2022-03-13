8ca9976a4558021c9c38be5d724da1761b1f7aa4
Елена <el_el_@mail.ru>
2022-03-13 18:36:38 +0300

    Модуляция2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import {getTestPost} from './post.js';
+import {getTestPost} from './get-post.js';
 const getTestPosts =  Array.from({length: 25}, (v,i) => getTestPost(i+1));
 // eslint-disable-next-line no-console
 console.log(getTestPosts);
