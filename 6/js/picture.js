e617e2a6071c00a55ca31aa3e3ab13a0b7386d59
Keks <keks@htmlacademy.ru>
2022-03-29 17:30:47 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/picture.js
@@ -0,0 +1,20 @@
+import {getTestPost} from './get-post.js';
+
+const showListPictures = document.querySelector('.pictures');
+const showPictureTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+const getTestPosts =  Array.from({length: 25}, (v,i) => getTestPost(i+1));
+
+
+getTestPosts.forEach(({url,likes,comments}) => {
+  const pictureView = showPictureTemplate.cloneNode(true);
+  pictureView.querySelector('.picture__img').src=url;
+  pictureView.querySelector('.picture__likes').textContent =likes;
+  pictureView.querySelector('.picture__comments').textContent = comments.length;
+  showListPictures.appendChild(pictureView);
+});
+
+
+export {showPictureTemplate, getTestPosts};
