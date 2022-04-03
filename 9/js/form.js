416d53dbecf476bc2ab4ab8840d93bb9f5cad93f
Keks <keks@htmlacademy.ru>
2022-04-03 21:09:14 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/form.js
@@ -0,0 +1,84 @@
+/* eslint-disable no-use-before-define */
+import { isEscapeKey } from './util.js';
+import { resetScale } from './scale-picture.js';
+import { resetFilter } from './filters-picture.js';
+
+const uploadForm = document.querySelector('.img-upload__form');
+const uploadFile = uploadForm.querySelector('#upload-file');
+const uploadOverlayForm = uploadForm.querySelector('.img-upload__overlay');
+const uploadOverlayCloseForm = uploadForm.querySelector('.img-upload__cancel');
+const imgUploadPreview = uploadForm.querySelector('.img-upload__preview img');
+const uploadTextDescription = uploadForm.querySelector('.text__description');
+const uploadTexthashtags = uploadForm.querySelector('.text__hashtags');
+const imgUploadPreviewEffects = uploadForm.querySelectorAll('.effects__preview');
+
+const pristine = new Pristine(uploadForm,  {
+  classTo: 'form__item',
+  errorClass: 'form__item--invalid',
+  successClass: 'form__item--valid',
+  errorTextParent: 'form__item',
+  errorTextTag: 'span',
+  errorTextClass: 'form__error'
+});
+
+const validateDescription = (textDescription) => textDescription.length <= 140;
+
+const validateHashtags = (textHashtags) => {
+  const arrayHashtags = textHashtags.split(' ');
+  const re = /^#[A-Za-zА-Яа-яЕё0-9]{1,19}$/;
+  return arrayHashtags.length === arrayHashtags.filter((hashTag) => re.test(hashTag)).length;
+};
+
+pristine.addValidator(uploadTextDescription,validateDescription,'До 140 симолов!');
+pristine.addValidator(uploadTexthashtags,validateHashtags,'Условия xэш-тегов!');
+
+uploadForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+});
+
+const onFormEscKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeFormModal();
+  }
+};
+
+const closeFormModal = () => {
+  document.body.classList.remove('modal-open');
+  uploadOverlayForm.classList.add('hidden');
+  document.removeEventListener('keydown', onFormEscKeydown);
+  uploadForm.reset();
+  pristine.reset();
+};
+
+uploadFile.addEventListener('change', () => {
+  document.body.classList.add('modal-open');
+  uploadOverlayForm.classList.remove('hidden');
+  imgUploadPreview.src = URL.createObjectURL(uploadFile.files[0]);
+  imgUploadPreviewEffects.forEach((image) => {
+    image.style.backgroundImage = `url(${imgUploadPreview.src})`;
+  });
+  document.addEventListener('keydown', onFormEscKeydown);
+  resetScale();
+  resetFilter();
+});
+
+uploadOverlayCloseForm.addEventListener('click',closeFormModal);
+
+uploadTextDescription.addEventListener('focus',() => {
+  document.removeEventListener('keydown', onFormEscKeydown);
+});
+
+uploadTextDescription.addEventListener('blur',() => {
+  document.addEventListener('keydown', onFormEscKeydown);
+});
+
+uploadTexthashtags.addEventListener('focus',() => {
+  document.removeEventListener('keydown', onFormEscKeydown);
+});
+
+uploadTexthashtags.addEventListener('blur',() => {
+  document.addEventListener('keydown', onFormEscKeydown);
+});
+
---
+++ 9/js/get-post.js
@@ -0,0 +1,19 @@
+import {MESSAGES, NAMES, DESCRIPTION} from './data.js';
+import {getRandomPositiveInteger, getRandomArrayElement } from  './util.js';
+
+const getTestComments = (commentIndex) =>({
+  id: `${commentIndex}`,
+  avatar: `img/avatar-${  getRandomPositiveInteger(1,6) }.svg`,
+  message: getRandomArrayElement(MESSAGES),
+  userName: getRandomArrayElement(NAMES),
+});
+
+const getTestPost = (postIndex) => ({
+  id:`${postIndex}`,
+  url: `photos/${postIndex}.jpg`,
+  description: getRandomArrayElement(DESCRIPTION),
+  likes: getRandomPositiveInteger(15,200),
+  comments: Array.from({length: getRandomPositiveInteger(0,20)}, (v,i) => getTestComments((postIndex*1000)+i))
+});
+
+export {getTestPost};
